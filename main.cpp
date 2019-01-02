c5ef693df1feb007365aa033800806a239a56632
rasimon <rasimon@smu.edu>
2019-01-01 23:54:42 -0600

    Minor code cleanup

--- main.cpp
+++ main.cpp
@@ -15,14 +15,14 @@
 
 int main() {
     Simulation::Projectile proj(Primitives::point(0, 1, 0), Primitives::vector(5, 5, 0));
-    Simulation::Environment env(Primitives::vector(0, -0.1, 0), Primitives::vector(0.0, 0.0, 0));
+    Simulation::Environment env(Primitives::vector(0, -0.1f, 0), Primitives::vector(0.0, 0.0, 0));
     std::cout << proj.getPosition() << std::endl;
     Render::Canvas canv(1000,200);
     Render::Color red(255.0, 0.0, 0.0);
     while(proj.getPosition().y > 0)
     {
         proj = Simulation::tick(env, proj);
-        std::cout << proj.getPosition() << std::endl;
+        //std::cout << proj.getPosition() << std::endl;
         if(proj.getPosition().y > 0 && proj.getPosition().x > 0){
             canv.at(static_cast<size_t>(proj.getPosition().x), 200 - static_cast<size_t>(proj.getPosition().y)) = red;
         }
